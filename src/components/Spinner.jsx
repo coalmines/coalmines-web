f8f4ce3b0a596fbf211a02ba00d5159ed8451f86
Daniel Maslowski <info@orangecms.org>
2020-06-12 23:37:37 +0200

    implement spinner types: spin, swing, and pendulum

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Spinner = () => {
+const Spinner = ({ children, type = "spin" }) => {
   return (
     <>
-      <div className="spin">Waiting for data...</div>
+      <div className={type}>{children}</div>
       <style jsx>{`
         @keyframes spin {
           0% {
@@ -14,9 +14,37 @@ const Spinner = () => {
           }
         }
         .spin {
-          width: 50%;
-          margin-top: 100px;
+          text-align: center;
           animation: spin 2s infinite linear;
+        }
+        @keyframes swing {
+          0% {
+            transform: rotate(30deg);
+          }
+          50% {
+            transform: rotate(-30deg);
+          }
+          100% {
+            transform: rotate(30deg);
+          }
+        }
+        .swing {
+          text-align: center;
+          animation: swing 2s infinite ease-in-out;
+        }
+        @keyframes pendulum {
+          0% {
+            transform: rotate(-60deg);
+          }
+          50% {
+            transform: rotate(-120deg);
+          }
+          100% {
+            transform: rotate(-60deg);
+          }
+        }
+        .pendulum {
+          animation: pendulum 2s infinite ease-in-out;
         }
       `}</style>
     </>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,7 +8,61 @@ export default () => {
   return (
     <div>
       <h1>Hello!</h1>
-      {loading ? <Spinner /> : <Data />}
+      {loading ? <Spinner>Loading...</Spinner> : <Data />}
+      <div className="spinners">
+        <div className="widget">
+          <div className="spin">
+            <Spinner type="spin">
+              You spin my head right round...
+            </Spinner>
+          </div>
+        </div>
+        <div className="widget">
+          <div className="swing">
+            <Spinner type="swing">
+              <span className="flex">
+                <div>x</div>
+                <div>x</div>
+              </span>
+              <hr />
+              <div>o</div>
+            </Spinner>
+          </div>
+        </div>
+        <div className="widget">
+          <div className="pendulum">
+            <Spinner type="pendulum">tic<hr />toc</Spinner>
+          </div>
+        </div>
+      </div>
+      <style jsx>{`
+        .spinners {
+          margin-top: 60px;
+        }
+        .widget {
+          width: 150px;
+          height: 100px;
+          margin: 60px 30px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          background-color: #CFCFCF;
+        }
+        .widget .spin {
+          width: 40%;
+        }
+        .widget .swing {
+          width: 80%;
+        }
+        .widget .pendulum {
+          width: 60%;
+        }
+        .flex {
+          display: flex;
+          justify-content: space-between;
+        }
+      `}
+      </style>
     </div>
   );
 };
