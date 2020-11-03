1ef8411314d958232742622a7b96a7421b60ccf0
Daniel Maslowski <info@orangecms.org>
2020-11-03 17:52:09 +0100

    fix asset prefix in config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-module.exports = {
-    basePath: '/web',
-    assetPrefix: '/web/',
+const cfg = {
+  basePath: "/web",
+};
+
+if (process.env.NODE_ENV === "production") {
+  cfg.assetPrefix = process.env.NODE_APP_PATH || "/web/";
 }
+
+module.exports = cfg;
