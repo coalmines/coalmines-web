a238a4b6688f90ba599bde072d6db5114e0ab97e
Daniel Maslowski <info@orangecms.org>
2020-11-04 17:06:44 +0100

    fix Next config for local development

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
-const cfg = {
-  basePath: "/web",
-};
+const cfg = {};
 
 if (process.env.NODE_ENV === "production") {
   cfg.assetPrefix = process.env.NODE_APP_PATH || "/web/";
+  cfg.basePath = "/web";
 }
 
 module.exports = cfg;
