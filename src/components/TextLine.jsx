34648eec0741141bc43dcf30575eb763f47e8269
Daniel Maslowski <info@orangecms.org>
2021-02-13 20:06:35 +0100

    design input and add onChange listener
    This raises the horizontal padding to 12px.

--- src/components/TextLine.jsx
+++ src/components/TextLine.jsx
@@ -1,52 +1,85 @@
 import React, { useState } from "react";
 import {
-  backgroundColor,
+  backgroundColor as regularBackgroundColor,
   borderColor as regularBorderColor,
   textColor,
   horizontalPadding,
 } from "../theme";
 
-const TextLine = ({ children, label = "", active, input }) => {
+const TextLine = ({
+  children,
+  label = "",
+  active,
+  input,
+  placeholder,
+  onEdit,
+}) => {
   const borderColor = active ? "#50C050" : regularBorderColor;
+  const backgroundColor = input ? "#FDFDFF" : regularBackgroundColor;
   const [value, setValue] = useState("some input");
-  const onChange = (e) => setValue(e.target.value);
+  const onChange = (e) => {
+    const { value } = e.target;
+    setValue(value);
+    onEdit && onEdit(value);
+  };
+  const prefix = input ? "✏️" : "";
   // FIXME: factor out input into its own component
   return (
     <>
-      <span className="text">
-        {(input && <input value={value} onChange={onChange} />) || children}
+      <span className="border">
+        {input ? (
+          <input placeholder={placeholder} value={value} onChange={onChange} />
+        ) : (
+          <div className="text">{children}</div>
+        )}
         {label !== "" && <span className="label">{label}</span>}
       </span>
       <style jsx>{`
-        input {
-          width: 160px;
-          height: 20px;
-        }
+        input,
         .text {
-          position: relative;
-          height: 20px;
-          display: inline-flex;
+          display: flex;
           align-items: center;
+          width: 100%;
+          height: 100%;
+          padding: 0 ${horizontalPadding};
+          font: 13px sans-serif;
+        }
+        input {
+          outline: none;
+          border: none;
           background: ${backgroundColor};
-          padding: 16px ${horizontalPadding};
+        }
+        .border {
+          display: inline-block;
+          position: relative;
+          height: 30px;
+          background: ${backgroundColor};
           border-bottom: 2px solid ${borderColor};
           border-left: 2px solid ${borderColor};
         }
-        .text::before {
+        .border::before {
           position: absolute;
           top: 0;
           left: 0;
           width: ${horizontalPadding};
           height: 100%;
-          content: "";
-          content: "";
           border-top: 2px solid ${borderColor};
+          display: inline-flex;
+          align-items: center;
+          font-size: 8px;
+          content: "${prefix}";
         }
         .label {
-          font-size: 10px;
+          width: calc(100% - ${horizontalPadding} + 2px);
+          height: calc(${horizontalPadding} + 2px);
+          border: 2px solid ${borderColor};
+          border-bottom: 0;
+          padding: 2px 6px;
+          background: ${backgroundColor};
+          font: 10px sans-serif;
           position: absolute;
-          left: 16px;
-          top: -5px;
+          left: calc(${horizontalPadding} - 2px);
+          top: -${horizontalPadding};
         }
       `}</style>
     </>
--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,4 @@
 export const backgroundColor = "#F4F4FC";
 export const borderColor = "#3030C0";
 export const textColor = "202020";
-export const horizontalPadding = "10px";
+export const horizontalPadding = "12px";
