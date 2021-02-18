fd84094878a7c2b7d613b27352d73dd3d98d3547
Daniel Maslowski <info@orangecms.org>
2021-02-18 23:12:27 +0100

    Storybook: add docs addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-actions"],
+  addons: ["@storybook/addon-actions", "@storybook/addon-docs"],
 };
