f7e63d675520a682b582fbebccb777aa1ae0d810
Daniel Maslowski <info@orangecms.org>
2021-02-19 21:12:46 +0100

    Babel: revert to previous config, fix for Storybook
    Storybook builds were broken. The assumed fix was only half working.
    Instead, just define `_JSXStyle` to fix the reference issue:
    `ReferenceError: _JSXStyle is not defined`
    
    See also
    https://spectrum.chat/styled-jsx/general/storybook-next-js-styled-jsx-styles-not-scoped-to-component~8a63d2a1-6b05-4a27-b839-c4a903eb6f43
    and https://triplecore.io/blog/next-typescript-storybook/.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,9 @@
+import _JSXStyle from "styled-jsx/style";
 import "./global.css";
+
+if (typeof global !== "undefined") {
+  Object.assign(global, { _JSXStyle });
+}
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
